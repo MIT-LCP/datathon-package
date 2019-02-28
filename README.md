268e11a8ad2ebe723d38641afbc11ff86923f704
Tom Pollard <tpollard@mit.edu>
2019-02-28 15:42:16 -0500

    add setup files

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# datathon-package
+# Package containing helper functions for datathons.
+
 Pip installable package containing helper functions for datathons
+
+## Installation
+
+```pip install tableone```
+
+## Notes for use
+
---
+++ setup.py
@@ -0,0 +1,87 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='datathon',
+
+    # Versions should comply with PEP440. For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.1.0',
+
+    description='datathon',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/MIT-LCP/datathon-package',
+
+    # # Author details
+    # author='Tom Pollard',
+    # author_email='tpollard@mit.edu',
+
+    # Choose your license
+    license='MIT',
+
+    # What does your project relate to?
+    keywords='datathon',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    # packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    # packages=['tableone'],
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    py_modules=['datathon'],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=[
+        'pandas>=0.22.0',
+        'numpy>=1.12.1',
+        ],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    # extras_require={
+    #     'dev': ['check-manifest'],
+    #     'test': ['coverage'],
+    # },
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    # package_data={'wfdb': ['wfdb.config'],
+    # },
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    # data_files=[('my_data', ['data/data_file'])],
+    # data_files=[('config', ['wfdb.config'])],
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    # entry_points={
+    #     'console_scripts': [
+    #         'sample=sample:main',
+    #     ],
+    # },
+
+)
